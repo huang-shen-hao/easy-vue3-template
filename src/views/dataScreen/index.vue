2bf83723e97cdeb3241beae74d20d954e57cc0f5
1467788588 <1467788588@qq.com>
2024-02-02 15:45:07 +0800

    tag:暂存图片资源

--- src/views/dataScreen/index.vue
+++ src/views/dataScreen/index.vue
@@ -1,7 +1,32 @@
 <template>
-  <div>数据大屏</div>
+  <div class="data_container"></div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const container = document.querySelector('.data_container')
 
-<style scoped lang="scss"></style>
+// 获取变化后 缩放比例
+const getScaleRatio = (w: 1920, h: 1080) => {
+  const W_Ratio = window.innerWidth / w
+  const H_Ratio = window.innerHeight / h
+  return W_Ratio < H_Ratio ? W_Ratio : H_Ratio
+}
+
+window.onresize = () => {
+  console.log('H', window.innerHeight)
+  console.log('W', window.innerWidth)
+  let ratio = getScaleRatio(1920, 1080)
+  console.log('ooo', ratio)
+}
+</script>
+
+<style scoped lang="scss">
+.data_container {
+  width: 100vw;
+  height: 100vh;
+  background-image: url('@/assets/bg.png');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+}
+</style>
