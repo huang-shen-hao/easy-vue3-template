a91ef291a35aa5b72947c4b708871bacea5480b6
1467788588 <1467788588@qq.com>
2024-02-04 21:51:25 +0800

    feat:404样式优化

--- src/views/404/index.vue
+++ src/views/404/index.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="container">
-    <el-row>
+    <el-row class="top_container">
       <div class="img_container"></div>
     </el-row>
-    <el-row>
+    <el-row class="bottom_container">
       <div class="text_container">
         <p>404 ERROR</p>
         <p>抱歉，您没有权限访问此页面</p>
       </div>
-    </el-row>
-    <el-row>
       <div class="button_container">
         <el-button type="primary" @click="router.push('/')">返回首页</el-button>
       </div>
@@ -25,64 +23,72 @@ import router from '@/router/index.js'
 .container {
   width: 100%;
   height: 100vh;
-  background-color: #d3a9a9;
-  .el-row {
+  background-color: #777dec;
+  .top_container {
+    width: 100%;
+    height: 60vh;
     display: flex;
     align-items: center;
     justify-content: center;
-  }
-  .img_container {
-    width: 800px;
-    height: 800px;
-    background: url(@/assets/404.png) no-repeat;
-    background-size: contain;
-  }
-  .text_container {
-    color: #f1eaea;
-    text-align: center;
-    font-size: 80px;
-  }
-  .button_container {
-    margin-top: 20px;
-
-    ::v-deep .el-button {
-      position: relative;
-      width: auto;
-      min-width: 180px;
-      height: 52px;
-      padding: 10px;
-      text-align: center;
-      color: #fff;
-      background: #00d463;
-      cursor: pointer;
-      border-radius: 4px;
-      border: none;
-    }
-
-    ::v-deep .el-button:after,
-    .el-button:before {
-      content: '';
-      position: absolute;
-      left: 0;
-      top: 0;
-      width: 0;
+    .img_container {
+      width: 600px;
       height: 100%;
-      background: #00d463;
-      z-index: -2;
-      border-radius: 4px;
+      background-size: cover;
+      background: url(@/assets/404.png) no-repeat center;
     }
-    ::v-deep .el-button:hover {
-      z-index: 1;
-      background: transparent;
+  }
+  .bottom_container {
+    width: 100%;
+    height: 40vh;
+    display: flex;
+    flex-direction: column;
+    //justify-content: center;
+    align-items: center;
+    .text_container {
+      color: #f1eaea;
+      text-align: center;
+      font-size: 60px;
     }
-    ::v-deep .el-button:before {
-      transition: 0.3s;
-      background: #14ae5c;
-      z-index: -1;
-    }
-    ::v-deep .el-button:hover:after,
-    .el-button:hover:before {
-      width: 100%;
+    .button_container {
+      ::v-deep .el-button {
+        margin-top: 20px;
+        position: relative;
+        width: 180px;
+        height: 52px;
+        padding: 10px;
+        text-align: center;
+        color: #fff;
+        background: #00d463;
+        cursor: pointer;
+        border-radius: 4px;
+        border: none;
+      }
+      ::v-deep .el-button:after,
+      .el-button:before {
+        content: '';
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 0;
+        height: 100%;
+        background: #00d463;
+        z-index: -2;
+        border-radius: 4px;
+      }
+      ::v-deep .el-button:hover {
+        z-index: 1;
+        background: transparent;
+        font-size: 18px;
+      }
+      ::v-deep .el-button:before {
+        transition: 0.3s;
+        background: rgb(98, 162, 243);
+        z-index: -1;
+      }
+      ::v-deep .el-button:hover:after,
+      .el-button:hover:before {
+        width: 100%;
+      }
     }
   }
 }
